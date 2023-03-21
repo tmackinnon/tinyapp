0183ba84f49118000060e196f753b1bd85896c29
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-21 05:00:15 +0000

    cleaned up code

--- express_server.js
+++ express_server.js
@@ -15,34 +15,28 @@ const generateRandonString = function() {
   return Math.random().toString(36).slice(2,8);
 }
 
+//GET SECTION
+
 //HOMEPAGE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
 
 //URLS PAGE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; //need to send these in an object
   res.render("urls_index", templateVars); //looks for urls_index.ejs file and gives it access to templateVars
 });
-
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  const newShortURL = generateRandonString();  //create random ID
-  urlDatabase[newShortURL] = req.body.longURL; //store data in urlDatabase object
-  //redirect to coressponding url page
-  res.redirect(`/urls/:${newShortURL}`)
-  });
 
 //NEW URLS 
 app.get("/urls/new", (req, res) => {
@@ -51,7 +45,8 @@ app.get("/urls/new", (req, res) => {
 
 //SPECIFIC URL PAGE
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }
+  const shortURL = req.params.id;
+  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL] }
   res.render("urls_show", templateVars);
 });
 
@@ -61,6 +56,17 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+
+//POST SECTION
+
+//POST ON URLS
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const newShortURL = generateRandonString();  //create random ID
+  urlDatabase[newShortURL] = req.body.longURL; //store data in urlDatabase object
+  //redirect to coressponding url page
+  res.redirect(`/urls/:${newShortURL}`)
+  });
 
 //app listen on port
 app.listen(PORT, () => {
